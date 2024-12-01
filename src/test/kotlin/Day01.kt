d40d296635b7b04d874b91fcf99fe0b0bf82c615
Christof Vollrath <christof@taobits.net>
2024-12-01 13:39:43 +0100

    Day01 part2 solved

--- src/test/kotlin/Day01.kt
+++ src/test/kotlin/Day01.kt
@@ -45,6 +45,74 @@ class Day01Part1: BehaviorSpec() { init {
     }
 } }
 
+class Day01Part2: BehaviorSpec() { init {
+
+    Given("example input") {
+        val notePairs = parseNotePairs(exampleInputDay01)
+        When("splitting in two lists") {
+            val splitPairs = splitPairs(notePairs)
+            Then("should be two lists") {
+                splitPairs.first[0] shouldBe 3
+                splitPairs.second[0] shouldBe 4
+            }
+            When("counting occurrences") {
+                val occurences = countOccurences(splitPairs.second)
+                Then("should have counted correctly") {
+                    occurences[3] shouldBe 3
+                }
+            }
+            When("calculating similarity scores") {
+                val similarityScores = calculateSimilarityScores(splitPairs)
+                Then("should have calculated similarity scores") {
+                    similarityScores[0] shouldBe (3 to 3)
+                }
+            }
+            When("caluclating total similarity scores") {
+                val total = calculateTotalSimilarityScore(splitPairs)
+                Then("it should have the right value") {
+                    total shouldBe 23_384_288L
+                }
+            }
+        }
+    }
+    Given("exercise input") {
+        val notePairs = parseNotePairs(readResource("inputDay01.txt")!!)
+        When("total similarity score") {
+            val splitPairs = splitPairs(notePairs)
+            val total = calculateTotalSimilarityScore(splitPairs)
+            Then("it should have the right value") {
+                total shouldBe 31L
+            }
+        }
+    }
+}
+
+    private fun calculateTotalSimilarityScore(listsPair: Pair<List<Int>, List<Int>>): Long =
+        calculateSimilarityScores(listsPair).map { it.first.toLong() * it.second.toLong() }.sum()
+
+    private fun calculateSimilarityScores(listsPair: Pair<List<Int>, List<Int>>): List<Pair<Int, Int>> {
+        val occurences = countOccurences(listsPair.second)
+        return sequence {
+            listsPair.first.forEach {
+                yield (it to (occurences[it] ?: 0))
+            }
+        }.toList()
+    }
+
+    private fun countOccurences(ints: List<Int>): Map<Int, Int> {
+        val counts = HashMap<Int, Int>()
+        ints.forEach {
+            val found = counts[it]
+            if (found == null) counts[it] = 1
+            else counts[it] = found + 1
+        }
+        return counts
+    }
+
+    private fun splitPairs(notePairs: List<Pair<Int, Int>>): Pair<List<Int>, List<Int>> =
+        notePairs.map { it.first } to notePairs.map { it.second }
+}
+
 private fun pairsDistance(pairs: List<Pair<Int, Int>>) = pairs.map { (it.first - it.second).absoluteValue }
 
 private fun parseNotePairs(input: String): List<Pair<Int, Int>> =
