7120fcfef7c63e7814366140f97652a6fddbcd0d
Christof Vollrath <christof@taobits.net>
2024-12-01 13:07:26 +0100

    Day01 part 1 solved

--- src/test/kotlin/Day01.kt
+++ src/test/kotlin/Day01.kt
@@ -1,13 +1,64 @@
 import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import kotlin.math.absoluteValue
 
-class Day01Part1: BehaviorSpec() { init {
+class Day01Part1: BehaviorSpec() {
+
+    init {
     val exampleInput = """
-
+        3   4
+        4   3
+        2   5
+        1   3
+        3   9
+        3   3
     """.trimIndent()
 
     Given("example input") {
-        Then("should do something") {
+        When("parsing note pairs") {
+            val notePairs = parseNotePairs(exampleInput)
+            Then("should be parsed") {
+                notePairs[0] shouldBe (3 to 4)
+            }
+            When("sorting the pairs indvidually") {
+                val sortedPairs = sortPairsIndividually(notePairs)
+                Then(" should be sorted") {
+                    sortedPairs[0] shouldBe (1 to 3)
+                }
+                When("summing the diffs") {
+                    val sum = pairsDistance(sortedPairs).sum()
+                    Then("should find the right value") {
+                        sum shouldBe 11
+                    }
+                }
+            }
         }
     }
-} }
+    Given("exercise input") {
+        val notePairs = parseNotePairs(readResource("inputDay01.txt")!!)
+        When("sort and sum diffs") {
+            val sortedPairs = sortPairsIndividually(notePairs)
+            val sum = pairsDistance(sortedPairs).sum()
+            Then("it should find the solution for part 1") {
+                sum shouldBe 2_176_849
+            }
+        }
+    }
+
+}
+
+    private fun pairsDistance(pairs: List<Pair<Int, Int>>) = pairs.map { (it.first - it.second).absoluteValue }
+
+    private fun parseNotePairs(input: String): List<Pair<Int, Int>> =
+        input.split("\n"). map {
+            val stringParts = it.trim().split("""\s+""".toRegex()).map { it.toInt() }
+            stringParts[0] to stringParts[1]
+        }
+
+    private fun sortPairsIndividually(notePairs: List<Pair<Int, Int>>): List<Pair<Int, Int>> {
+        val list1 = notePairs.map { it.first }.sorted()
+        val list2 = notePairs.map { it.second }.sorted()
+        return list1.zip(list2)
+    }
+}
 
