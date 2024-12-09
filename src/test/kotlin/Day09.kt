47ea58f4bf1b6dae2f84b317b014e302a3b2c248
Christof Vollrath <christof@taobits.net>
2024-12-10 00:35:08 +0100

    Day09 part2 solved

--- src/test/kotlin/Day09.kt
+++ src/test/kotlin/Day09.kt
@@ -9,11 +9,11 @@ class Day09Part1: BehaviorSpec() { init {
         val blocks = parseDiskBlocks("12345")
         Then("should have right blocks") {
             blocks shouldBe listOf(
-                Block(0, 0, 1, false),
-                Block(-1, 1, 2, true),
-                Block(1, 3, 3, false),
-                Block(-1, 6, 4, true),
-                Block(2, 10, 5, false)
+                Block(0,  1, false),
+                Block(-1, 2, true),
+                Block(1, 3, false),
+                Block(-1, 4, true),
+                Block(2, 5, false)
             )
         }
         Then("should print blocks") {
@@ -75,20 +75,72 @@ class Day09Part1: BehaviorSpec() { init {
                 sumIds(compacted) shouldBe 6_366_665_108_136L
             }
         }
+    }
+} }
+
+class Day09Part2: BehaviorSpec() { init {
+
+    Given("more examples") {
+        When("the last block fits completely in a free block at the begining") {
+            val example = parseDiskBlocks("12122")
+            printBlocks(example) shouldBe "0..1..22"
+            Then("should be compacted correctly") {
+                printBlocks(compactBlocks2(example)) shouldBe "0221...."
+            }
+        }
+        When("the last block doesn't fit completely in a free block at the begining") {
+            val example = parseDiskBlocks("12123")
+            printBlocks(example) shouldBe "0..1..222"
+            Then("should not move last block") {
+                printBlocks(compactBlocks2(example)) shouldBe "01....222"
+            }
+        }
+        When("the last block is smaller than the free block at the begining") {
+            val example = parseDiskBlocks("12121")
+            printBlocks(example) shouldBe "0..1..2"
+            Then("should be compacted correctly") {
+                printBlocks(compactBlocks2(example)) shouldBe "021...."
+            }
+        }
+    }
+    Given("example input") {
+        val blocks = parseDiskBlocks(exampleInputDay09)
+        When("compacting") {
+            val compacted = compactBlocks2(blocks)
+            Then("should be compacted correctly") {
+                printBlocks(compacted) shouldBe "00992111777.44.333....5555.6666.....8888.."
+            }
+            Then("should have the right checksum") {
+                sumIds(compacted) shouldBe 2858L
+            }
+        }
+    }
+
+    Given("exercise input") {
+        val blocks = parseDiskBlocks(readResource("inputDay09.txt")!!)
+        Then("sum of all block sizes") {
+            blocks.map { it.size }.sum() shouldBe 94935
+
+        }
+        When("compacting") {
+            val compacted = compactBlocks2(blocks)
+
+            Then("should have the right checksum") {
+                sumIds(compacted) shouldBe 6_398_065_450_842L
+            }
+        }
 
     }
 } }
 
 private fun parseDiskBlocks(input: String): List<Block> {
-    var memPointer = 0
     var empty = false
     var id = 0
     return input.mapIndexedNotNull() { i, c ->
         val length = c.toString().toInt()
         val result = if (! (empty && length == 0)) // empty block with size 0 not in result
-            Block(if (empty) -1 else id, memPointer, length, empty)
+            Block(if (empty) -1 else id, length, empty)
         else null
-        memPointer += length
         if (!empty) id++ // count only not empty blocks
         empty = !empty // empty blocks alternate
         result
@@ -103,16 +155,14 @@ private fun printBlocks(blocks: List<Block>): String = sequence {
     } }
 }.joinToString("")
 
+
 private fun sumIds(blocks: List<Block>): Long =
     blocks.flatMap { with(it) {
-            if (!empty) List(size) { id.toLong() }
-            else emptyList()
+            List(size) { if (empty) 0 else id.toLong() }
         }
     }.mapIndexed { i, size ->
         i * size
     }.sum()
-
-
 
 private fun compactBlocks(blocks: List<Block>): List<Block> {
     val compactedBlocks  = mutableListOf<Block>()
@@ -142,14 +192,14 @@ private fun compactBlocks(blocks: List<Block>): List<Block> {
                     fromEndBlock = null
                     fromEnd--
                 } else if (fromEndBlock.size < fromStartBlock.size) { // block smaller than free space
-                    compactedBlocks.add(Block(fromEndBlock.id, 0, fromEndBlock.size, false))
-                    freeBlocks.add(Block(-1, 0, fromEndBlock.size, true))
+                    compactedBlocks.add(Block(fromEndBlock.id, fromEndBlock.size, false))
+                    freeBlocks.add(Block(-1, fromEndBlock.size, true))
                     fromStartBlock = fromStartBlock.copy(size = fromStartBlock.size - fromEndBlock.size)
                     fromEndBlock = null
                     fromEnd--
                 } else { // empty block not big enough
-                    compactedBlocks.add(Block(fromEndBlock.id, 0, fromStartBlock.size, false))
-                    freeBlocks.add(Block(-1, 0, fromStartBlock.size, true))
+                    compactedBlocks.add(Block(fromEndBlock.id, fromStartBlock.size, false))
+                    freeBlocks.add(Block(-1, fromStartBlock.size, true))
                     fromEndBlock = fromEndBlock.copy(size = fromEndBlock.size - fromStartBlock.size)
                     fromStartBlock = null
                     fromStart++
@@ -167,5 +217,36 @@ private fun compactBlocks(blocks: List<Block>): List<Block> {
     return compactedBlocks + freeBlocks
 }
 
-private data class Block(val id: Int, val address: Int, val size: Int, val empty: Boolean)
+private fun compactBlocks2(blocks: List<Block>): List<Block> {
+    var idLastBlockChecked = Int.MAX_VALUE
+    var currentBlocks = blocks
+    val movedBlocks = mutableSetOf<Int>()
+    while (true) {
+        val indexBlockToMove = currentBlocks.indexOfLast { !it.empty && it.id < idLastBlockChecked }
+        if (indexBlockToMove < 0) return currentBlocks
+        val blockToMove = currentBlocks[indexBlockToMove]
+        idLastBlockChecked = blockToMove.id
+        if (movedBlocks.contains(blockToMove.id)) return currentBlocks // block to move has already been moved
+        val indexMoveTo = currentBlocks.indexOfFirst { it.empty && it.size >= blockToMove.size }
+        if (indexMoveTo >= 0 && indexMoveTo < indexBlockToMove) {
+            val blockMoveTo = currentBlocks[indexMoveTo]
+            // Move block
+            val nextBlocks = sequence {
+                currentBlocks.forEachIndexed { i, block ->
+                    if (i == indexMoveTo) {
+                        yield(blockToMove)
+                        if (blockToMove.size < blockMoveTo.size)
+                            yield(Block(-1, blockMoveTo.size - blockToMove.size, true))
+                    } else if (i == indexBlockToMove) {
+                        yield(Block(-1, blockToMove.size, true))
+                    } else yield(block)
+                }
+            }.toList()
+            movedBlocks.add(blockToMove.id)
+            currentBlocks = nextBlocks
+        }
+    }
+}
+
+private data class Block(val id: Int, val size: Int, val empty: Boolean)
 
