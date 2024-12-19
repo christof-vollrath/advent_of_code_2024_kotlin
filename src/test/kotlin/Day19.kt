3a21a2971da165940abff73d254a8209dd594896
Christof Vollrath <christof@taobits.net>
2024-12-19 19:40:32 +0100

    Day19 part2 solved

--- src/test/kotlin/Day19.kt
+++ src/test/kotlin/Day19.kt
@@ -1,5 +1,7 @@
 import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.longs.shouldBeGreaterThan
 import io.kotest.matchers.shouldBe
+import java.lang.IllegalStateException
 
 val exampleInputDay19 = """
             r, wr, b, g, bwu, rb, gb, br
@@ -48,6 +50,60 @@ class Day19Part1: BehaviorSpec() { init {
     }
 }}
 
+class Day19Part2: BehaviorSpec() { init {
+
+    Given("example") {
+        val (towels, designs) = parseInputDay19(exampleInputDay19)
+        When("single example") {
+            val design = "gbbr"
+            Then("searching designs") {
+                val possibleDesign = findPossibleDesign2(design, towels)
+                possibleDesign.size shouldBe 4
+            }
+            Then("only counting") {
+                val count = countPossibleDesign(design, towels)
+                count shouldBe 4
+            }
+        }
+        When("another example") {
+            val design = "rrbgbr"
+            Then("searching designs") {
+                val possibleDesign = findPossibleDesign2(design, towels)
+                possibleDesign.size shouldBe 6
+            }
+            Then("only counting") {
+                val count = countPossibleDesign(design, towels)
+                count shouldBe 6
+            }
+        }
+        When("searching for all possible designs") {
+            val possibleDesign = findPossibleDesigns2(designs, towels)
+            val result = possibleDesign.map { (_, solutions) -> solutions.count() }.sum()
+            Then("should find 16") {
+                result shouldBe 16
+            }
+        }
+        When("just counting possible designs") {
+            val counts = countPossibleDesigns(designs, towels)
+            Then("should also find 16") {
+                counts shouldBe listOf(2, 1, 4, 6, 0, 1, 2, 0)
+            }
+        }
+    }
+
+    Given("exercise input") {
+        val (towels, designs) = parseInputDay19(readResource("inputDay19.txt")!!)
+        When("counting all possible designs") {
+            val counts = countPossibleDesigns(designs, towels)
+            Then("should have the right sum") {
+                val sum = counts.sum()
+                sum shouldBeGreaterThan 7951858249L
+                sum shouldBe 692575723305545L
+            }
+        }
+    }
+}}
+
 private fun parseInputDay19(input: String): Pair<List<String>, List<String>> {
     val (part1, part2) = input.split("\n\n")
     return part1.split(""",\s*""".toRegex()) to part2.split("\n")
@@ -87,4 +143,71 @@ private fun findPossibleDesign(design: String, towels: List<String>): List<Strin
         // println("searching next ${nextSearching.map {it.key}}")
         currSearching = nextSearching
     }
+}
+
+private fun findPossibleDesigns2(designs: List<String>, towels: List<String>): List<Pair<String, List<List<String>>>> =
+    designs.map { design ->
+        //println("Searching towels for $design")
+        val towelsForDesign = findPossibleDesign2(design, towels)
+        //println("Found ${towelsForDesign.size} for $design")
+        design to towelsForDesign
+    }
+
+private fun findPossibleDesign2(design: String, towels: List<String>): List<List<String>> {
+    val solution = mutableListOf<List<String>>()
+    var currSearching = mutableListOf<Pair<String, List<String>>>()
+    towels.forEach { towel ->
+        if (design.startsWith(towel)) currSearching.add(towel to mutableListOf(towel))
+    }
+    while(true) {
+        val nextSearching = mutableListOf<Pair<String, List<String>>>()
+        for (curr in currSearching) {
+            for (nextTowel in towels) {
+                val nextDesign = curr.first + nextTowel
+                val nextTowels = curr.second + nextTowel
+                if (nextDesign == design) { // found!
+                    solution.add(nextTowels)
+                }
+                if (nextDesign.length < design.length && design.startsWith(nextDesign)) {
+                    nextSearching.add(nextDesign to nextTowels)
+                }
+            }
+        }
+        if (nextSearching.isEmpty()) return solution // nothing more to find
+        //println("searching next ${nextSearching.map {it.first}}")
+        currSearching = nextSearching
+    }
+}
+
+private fun countPossibleDesigns(designs: List<String>, towels: List<String>): List<Long> =
+    designs.map { design ->
+        //println("Searching towels for $design")
+        val count = countPossibleDesign(design, towels)
+        //println("Found $count for $design")
+        count
+    }
+
+private fun countPossibleDesign(design: String, towels: List<String>): Long {
+    val counts = mutableMapOf<Int, Pair<Long, String>>() // contains counts up to pos
+    towels.forEach { towel ->
+        if (design.startsWith(towel)) {
+            counts[towel.length] = 1L to design.substring(towel.length)
+        }
+    }
+    for (i in 1 until design.length) {
+        if(counts.contains(i)) {
+            val (count, remaining) = counts[i]!!
+            towels.forEach { towel ->
+                if (remaining.startsWith(towel)) {
+                    val next = i + towel.length
+                    val existing = counts[next]
+                    if (existing != null) {
+                        if (remaining.substring(towel.length) != existing.second) throw IllegalStateException("expected $remaining at $i but found ${existing.second}")
+                        counts[next] = existing.first + count to existing.second
+                    } else counts[next] = count to remaining.substring(towel.length)
+                }
+            }
+        }
+    }
+    return counts[design.length]?.first ?: 0L
 }
