db80a979d40342288ff1cd248c64df1e2b4cbbf3
Christof Vollrath <christof@taobits.net>
2024-12-23 23:36:42 +0100

    Day23 part2 solved

--- src/test/kotlin/Day23.kt
+++ src/test/kotlin/Day23.kt
@@ -1,5 +1,7 @@
+
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.collections.shouldContain
+import io.kotest.matchers.ints.shouldBeGreaterThan
 import io.kotest.matchers.shouldBe
 
 val exampleInputDay23 = """
@@ -39,7 +41,7 @@ val exampleInputDay23 = """
 
 class Day23Part1: BehaviorSpec() { init {
 
-    Given("exampl") {
+    Given("example") {
         val connections = parseConnections(exampleInputDay23)
         Then("connections should be parsed") {
             connections.size shouldBe 32
@@ -56,7 +58,7 @@ class Day23Part1: BehaviorSpec() { init {
             computersWithConnections["td"]!! shouldContain("yn")
             computersWithConnections["yn"]!! shouldContain("td")
         }
-        val triples = findConnectedTriples(computersWithConnections)
+        val triples = findConnectedNLets(computersWithConnections)
         Then("should find right triples") {
             triples.size shouldBe 12
             triples shouldContain setOf("co", "de", "ka")
@@ -72,13 +74,61 @@ class Day23Part1: BehaviorSpec() { init {
         Then("should be parsed") {
             connections.size shouldBe 3380
         }
+        val allComputers = extractAllComputers(connections)
+        allComputers.size shouldBeGreaterThan 10
         val computersWithConnections = findComputersWithConnections(connections)
-        val triples = findConnectedTriples(computersWithConnections)
+        val triples = findConnectedNLets(computersWithConnections)
         val triplesWithT = triples.filter { computers -> computers.any { it.startsWith("t") } }
         Then("should find right triples with t") {
             triplesWithT.size shouldBe 1344
         }
     }
+}}
+
+class Day23Part2: BehaviorSpec() { init {
+    Given("some cases for nlets") {
+        val input1 = listOf("A", "B")
+        Then("nlet 1") {
+            createNLets(input1, 1) shouldBe listOf(listOf("A"), listOf("B"))
+        }
+        val input2 = listOf("A", "B")
+        Then("nlet 2") {
+            createNLets(input2, 2) shouldBe listOf(listOf("A", "B"))
+        }
+        val input3 = listOf("A", "B", "C")
+        Then("nlet 2 of 3") {
+            createNLets(input3, 2) shouldBe listOf(listOf("A", "B"), listOf("A", "C"), listOf("B", "C"))
+        }
+        val input4 = listOf("A", "B", "C", "D")
+        Then("nlet 3 of 4") {
+            createNLets(input4, 3) shouldBe listOf(
+                listOf("A", "B", "C"), listOf("A", "B", "D"),
+                listOf("A", "C", "D"),
+                listOf("B", "C", "D")
+            )
+        }
+    }
+    Given("example") {
+        val connections = parseConnections(exampleInputDay23)
+        val computersWithConnections = findComputersWithConnections(connections)
+        val nlets = findBiggestNLet(computersWithConnections)
+        Then ("nlet should be found") {
+            nlets.size shouldBe 1
+            nlets.first().size shouldBe 4
+            nlets.first() shouldBe listOf("co", "de", "ka", "ta")
+        }
+    }
+    xGiven("exercise input") { // runs for about 40 sec
+        val connections = parseConnections(readResource("inputDay23.txt")!!)
+        val computersWithConnections = findComputersWithConnections(connections)
+        val nlets = findBiggestNLet(computersWithConnections)
+        Then ("nlet should be found") {
+            nlets.size shouldBe 1
+            nlets.first().size shouldBe 13
+            nlets.first().joinToString(",") shouldBe "ab,al,cq,cr,da,db,dr,fw,ly,mn,od,py,uh"
+        }
+    }
+
 }}
 
 private fun parseConnections(input: String) = input.split("\n").map { line ->
@@ -102,33 +152,29 @@ private fun findComputersWithConnections(connections: List<Pair<String, String>>
     return result
 }
 
-private fun findConnectedTriples(computersWithConnections: Map<String, List<String>>): Set<Set<String>> {
-    val computersWithAtLeast3Connections = computersWithConnections.filter { it.value.size >= 3 }
-    val computersConnectedBack = computersWithAtLeast3Connections.map { listOf(it.key) + it.value.filter { connected -> computersWithAtLeast3Connections[connected]?.contains(it.key) ?: false }}
-    val reduced = reduceToInterconnected(computersConnectedBack, computersWithAtLeast3Connections)
+private fun findConnectedNLets(computersWithConnections: Map<String, List<String>>, nr: Int = 3): Set<Set<String>> {
+    val computersWithAtLeastNConnections = computersWithConnections.filter { it.value.size >= nr }
+    val computersConnectedBack = computersWithAtLeastNConnections.map { listOf(it.key) + it.value.filter { connected -> computersWithAtLeastNConnections[connected]?.contains(it.key) ?: false }}
+    val reduced = reduceToInterconnected(computersConnectedBack, computersWithAtLeastNConnections, nr)
     return reduced.toSet()
 }
 
-private fun reduceToInterconnected(computersList: List<List<String>>, connections: Map<String, List<String>>) = computersList.flatMap { computers ->
-    createTriples(computers).filter { checkConnections(it, connections) }.map { it.toSet() }
+private fun reduceToInterconnected(computersList: List<List<String>>, connections: Map<String, List<String>>, nr: Int) = computersList.flatMap { computers ->
+    createNLets(computers, nr).filter { checkConnections(it, connections) }.map { it.toSet() }
 }
 
-private fun createTriples(computers: List<String>) = sequence {
-    var remaining_1 = computers
-    while (remaining_1.size >= 3) {
-        val first = computers.first()
-        var remaining_2 = computers.drop(1)
-        while (remaining_2.size >= 2) {
-            val second = remaining_2.first()
-            val remaining_3 = remaining_2.drop(1)
-                for (third in remaining_3) {
-                    yield(listOf(first, second, third))
-                }
-            remaining_2 = remaining_2.drop(1)
-        }
-        remaining_1 = remaining_1.drop(1)
+private fun createNLets(computers: List<String>, n: Int = 3): List<List<String>> =
+    if (n == 0) emptyList<List<String>>()
+    else if (n == 1) computers.map { listOf(it) }
+    else if (n == computers.size) listOf(computers)
+    else {
+        val result = mutableListOf<List<String>>()
+        val nexts = createNLets(computers.drop(1), n - 1)
+        for (next in nexts) result.add(listOf(computers.first()) + next)
+        result.addAll(createNLets(computers.drop(1), n))
+        result
     }
-}.toList()
+
 
 private fun checkConnections(computers: List<String>, connections: Map<String, List<String>>): Boolean {
     var curr = computers.first()
@@ -141,4 +187,16 @@ private fun checkConnections(computers: List<String>, connections: Map<String, L
         remaining = remaining.drop(1)
     }
     return true
+}
+
+private fun findBiggestNLet(computersWithConnections: Map<String, List<String>>): Set<List<String>> {
+    var nr = 1
+    var curr: Set<Set<String>> = emptySet()
+    while(true) {
+        //println("checking $nr")
+        val next = findConnectedNLets(computersWithConnections, nr)
+        if (next.isEmpty()) return curr.map { it.sorted() }.toSet()
+        curr = next
+        nr++
+    }
 }
